b5a0c218d4279d8866dd6e79d2c6d11933912c54
prasam jain <90105063+pj1807@users.noreply.github.com>
2022-05-22 16:57:18 +0530

    Update create_insert_delet_Singly-circular_linked_list.cpp

--- Linked list/create_insert_delet_Singly-circular_linked_list.cpp
+++ Linked list/create_insert_delet_Singly-circular_linked_list.cpp
@@ -1,4 +1,4 @@
-//circular linked list using c/c++
+//Singly circular linked list using c/c++.
 
 //Circular linked list is a linked list where all nodes are connected to form a circle. 
 //There is no NULL at the end. A circular linked list can be a singly circular linked list or doubly circular linked list.
