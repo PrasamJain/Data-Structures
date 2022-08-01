179ca17ae93bace9ffb573fa91b5f6c657a554c7
pj1807 <prasamjain294@gmail.com>
2022-08-01 15:16:56 +0530

    find middle

--- Linked list/Middle element using TortoiseHare.cpp
+++ Linked list/Middle element using TortoiseHare.cpp
@@ -2,34 +2,36 @@
 
 /*
  * Definition for singly-linked list somthing like that.
+ 
  * struct Node {
  *     int val;
  *     Node *next;
- *     Node() : val(0), next(nullptr) {}
- *     Node(int x) : val(x), next(nullptr) {}
  *     Node(int x, Node *next) : val(x), next(next) {}
  * };
 */
  
+// we only write the function - "middleNode()" to find middle element of linked list
 
-class Solution {
-public:
-    Node* middleNode(Node* head) {
-        
-        Node *slow=head;
-        Node *fast=head;
-        if(head == NULL)
-        {
-            return head;
-        }
-        else
-        {
-            while(fast != NULL and fast->next != NULL)
-            {
-                slow = slow->next;
-                fast = fast->next->next;
-            }
-            return slow;
-        }
-    }
+class Solution 
+{
+	public:
+	    Node* middleNode(Node* head)      // pass head pointer.
+		{
+	        
+	        Node *slow=head;
+	        Node *fast=head;
+	        if(head == NULL)
+	        {
+	            return head;
+	        }
+	        else
+	        {
+	            while(fast != NULL and fast->next != NULL)
+	            {
+	                slow = slow->next;
+	                fast = fast->next->next;
+	            }
+	            return slow;        // returning middle pointer.
+	        }
+	    }
 };
